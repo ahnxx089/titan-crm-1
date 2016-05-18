9719429c2a703e4bf42da7f0e868e324551f9d12
Eric <brichett13@gmail.com>
2016-05-17 23:53:52 -0500

    pushing up Account and Organization

--- src/entities/account.js
+++ src/entities/account.js
@@ -6,3 +6,96 @@
 // @file:   account.js
 // @author: 
 /////////////////////////////////////////////////
+
+var validation = require('../common/validation')();
+var Organization = require('../entities/organization')();
+
+// Constructor
+//
+function Organization(partyId, createdDate, updatedDate, orgName, officeSiteName, annualRevenue,
+                numEmployees, tickerSymbol, comments,  logoImgURL,
+                      dummyattr1, dummyattr2, dummyattr3, dummyattr4) {
+    // Call the parent constructor first
+    Organization.call(this, partyId, createdDate, updatedDate, orgName, officeSiteName, annualRevenue, numEmployees, tickerSymbol, comments, logoImgUrl);
+    
+    // Properties specific to Account 
+    this.dummyattr1 = dummyattr1;
+    this.dummyattr2 = dummyattr2;
+    this.dummyattr3 = dummyattr3;
+    this.dummyattr4 = dummyattr4;
+}
+
+Account.prototype = Object.create(Organization.prototype);
+Account.prototype.constructor = Account;
+
+// Methods
+//
+Organization.prototype.validateForInsert = function () {
+    // Call Party's validation function
+    var errors = [Organization.prototype.validateForInsert.call(this)];
+    // Organization-specific validation code
+    // FILL THESE NAMES IN ONCE YOU KNOW THE EXACT FIELDS
+    var specificvalidations = [
+        this.dummyattr1 = dummyattr1(true),
+        this.dummyattr2 = dummyattr2(true),
+        this.dummyattr3 = dummyattr3(true),
+        this.dummyattr4 = dummyattr4(false)
+    ];
+    
+    for (var i = 0; i < validation.length; i++) {
+        if(specificvalidations[i] == true) {
+            errors[i].push(specificvalidations[i]);
+        }
+    };
+    return errors;
+};
+
+Account.prototype.validateForUpdate = function () {
+    // Call Party's validation function
+    var errors = [Organization.prototype.validateForUpdate.call(this)];
+    // Person-specific validation code
+    
+    var specificvalidations = [
+        this.dummyattr1 = dummyattr1(true),
+        this.dummyattr2 = dummyattr2(true),
+        this.dummyattr3 = dummyattr3(true),
+        this.dummyattr4 = dummyattr4(false)   
+    ];
+    
+    for (var i = 0; i < validations.length; i++) {
+        if(specificvalidations[i] == true) {
+            errors.push(specificvalidations[i]);
+        }
+    };
+    return errors;
+};
+
+
+
+
+Account.prototype.validateDummyAttr1 = function(isRequired) {
+    this.dummyattr1 = validation.sanitizeInput(this.dummyattr1);
+    var validationResult = validation.validateString(this.dummyattr1, isRequired, 40, 'dummyattr1');
+    return validationResult;
+};
+
+Account.prototype.validatedummyattr2 = function(isRequired) {
+    this.dummyattr2 = validation.sanitizeInput(this.dummyattr2);
+    var validationResult = validation.validateString(this.dummyattr2, isRequired, 40, 'dummyattr2');
+    return validationResult;
+};
+
+Account.prototype.validatedummyattr3 = function(isRequired) {
+    this.dummyattr3 = validation.sanitizeInput(this.dummyattr3);
+    var validationResult = validation.validateString(this.dummyattr3, isRequired, 20, 'dummyattr3');
+    return validationResult;
+};
+
+Account.prototype.validatedummyattr4 = function(isRequired) {
+    this.dummyattr4 = validation.sanitizeInput(this.dummyattr4);
+    var validationResult = validation.validateString(this.dummyattr4, isRequired, 20, 'dummyattr4');
+    return validationResult;
+};
+
+
+
--- src/entities/organization.js
+++ src/entities/organization.js
@@ -6,3 +6,122 @@
 // @file:   organization.js
 // @author: 
 /////////////////////////////////////////////////
+
+var validation = require('../common/validation')();
+var Party = require('../entities/party')();
+
+// Constructor
+//
+function Organization(partyId, createdDate, updatedDate, orgName, officeSiteName, annualRevenue,
+                numEmployees, tickerSymbol, comments,  logoImgURL) {
+    // Call the parent constructor first
+    Party.call(this, partyId, createdDate, updatedDate);
+    
+    // Properties specific to Organization 
+    this.orgName = orgName;
+    this.officeSiteName = officeSiteName;
+    this.annualRevenue = annualRevenue;
+    this.numEmployees = numEmployees;
+    this.tickerSymbol = tickerSymbol;
+    this.comments = comments;
+    this.logoImgURL = logoImgURL;
+    this.createdDate = createdDate;
+    this.updatedDate = updatedDate;
+}
+
+Organization.prototype = Object.create(Party.prototype);
+Organization.prototype.constructor = Organization;
+
+// Methods
+//
+Organization.prototype.validateForInsert = function () {
+    // Call Party's validation function
+    var errors = [Party.prototype.validateForInsert.call(this)];
+    // Organization-specific validation code
+    var specificvalidations = [
+        this.validateOrgName(true),
+        this.validateOfficeSiteName(true),
+        this.validateAnnualRevenue(false),
+        this.validateNumEmployees(false),
+        this.validateTickerSymbol(false),
+        this.validateComments(false),
+        this.validateLogoImgURL(false)
+    ];
+    
+    for (var i = 0; i < validation.length; i++) {
+        if(specificvalidations[i] == true) {
+            errors[i].push(specificvalidations[i]);
+        }
+    };
+    return errors;
+};
+
+Organization.prototype.validateForUpdate = function () {
+    // Call Party's validation function
+    var errors = [Party.prototype.validateForUpdate.call(this)];
+    // Person-specific validation code
+    
+    var specificvalidations = [
+        this.validateOrgName(true),
+        this.validateOfficeSiteName(true),
+        this.validateAnnualRevenue(false),
+        this.validateNumEmployees(false),
+        this.validateTickerSymbol(false),
+        this.validateComments(false),
+        this.validateLogoImgURL(false)   
+    ];
+    
+    for (var i = 0; i < validations.length; i++) {
+        if(specificvalidations[i] == true) {
+            errors.push(specificvalidations[i]);
+        }
+    };
+    return errors;
+};
+
+
+
+
+Organization.prototype.validateOrgName = function(isRequired) {
+    this.orgName = validation.sanitizeInput(this.orgName);
+    var validationResult = validation.validateString(this.orgName, isRequired, 40, 'orgName');
+    return validationResult;
+};
+
+Organization.prototype.validateOfficeSiteName = function(isRequired) {
+    this.officeSiteName = validation.sanitizeInput(this.officeSiteName);
+    var validationResult = validation.validateString(this.officeSiteName, isRequired, 40, 'officeSiteName');
+    return validationResult;
+};
+
+Organization.prototype.validateAnnualRevenue = function(isRequired) {
+    this.annualRevenue = validation.sanitizeInput(this.annualRevenue);
+    var validationResult = validation.validateString(this.annualRevenue, isRequired, 20, 'annualRevenue');
+    return validationResult;
+};
+
+Organization.prototype.validateNumEmployees = function(isRequired) {
+    this.numEmployees = validation.sanitizeInput(this.numEmployees);
+    var validationResult = validation.validateString(this.numEmployees, isRequired, 20, 'numEmployees');
+    return validationResult;
+};
+
+Organization.prototype.validateTickerSymbol = function(isRequired) {
+    this.tickerSymbol = validation.sanitizeInput(this.tickerSymbol);
+    var validationResult = validation.validateString(this.tickerSymbol, isRequired, 100, 'tickerSymbol');
+    return validationResult;
+};
+
+Organization.prototype.validateComments = function(isRequired) {
+    this.comments = validation.sanitizeInput(this.comments);
+    var validationResult = validation.validateString(this.comments, isRequired, 100, 'comments');
+    return validationResult;
+};
+
+Organization.prototype.validateLogoImgURL = function(isRequired) {
+    this.logoImgURL = validation.sanitizeInput(this.logoImgURL);
+    var validationResult = validation.validateString(this.logoImgURL, isRequired, 100, 'logoImgURL');
+    return validationResult;
+};
+
+
