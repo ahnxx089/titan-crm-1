7cfe60a78114b6a91a49ff5a47a9c8bdf8ceff5b
dpshenoy <dpshenoy@comcast.net>
2016-06-21 00:11:36 -0500

    UPDATED:  FindContactsPage now uses jquery-ui accordion to present user with one or the other search option. Please note:  To get the accordion to work requires installing the npm package called jquery-ui.  I added it to the package.json file, so after pulling down this commit if you do npm install, it should pick just that one additional package and add it to your local node_modules/ directory.

--- src/views/components/contacts/find-contacts/SearchForm.js
+++ src/views/components/contacts/find-contacts/SearchForm.js
@@ -1,72 +1,86 @@
 /////////////////////////////////////////////////
-// Search form component.
-// NOTE:  This may be replaced, pending working out accordion
-// For now it has both get by ID and by first/last name
+// Search form component
 //
 // @file:   SearchForm.js
 // @author: Dinesh Shenoy <astroshenoy@gmail.com>
 /////////////////////////////////////////////////
 
 var React = require('react');
+var ReactDOM = require('react-dom');
 
 var SearchForm = React.createClass({
 
+    componentDidMount: function(){
+        $('#accordion').accordion();    // for jquery-ui accordion
+    },
+    
     render: function(){
-
+        
         /* jshint ignore:start */
+        
         return(
             <form onSubmit={ this.props.onFormSubmit } onReset={ this.props.onFormReset }>
-                <div className="row">
-                    <div className="col-lg-4 col-xs-12">
-                        <div className="form-group">
-                            <label htmlFor="partyId">ID</label>
-                            <div className="input-group">
-                                <div className="input-group-addon">
-                                    <i className="fa fa-file-text-o" aria-hidden="true"></i>
-                                </div>
-                                <input type="text" 
-                                    className="form-control" 
-                                    id="partyId" 
-                                    placeholder="123" 
-                                    onChange={ this.props.onChange } 
-                                    value={ this.props.searchBy.partyId } />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-xs-12">
-                        <div className="form-group">
-                            <label htmlFor="firstName">First Name</label>
-                            <div className="input-group">
-                                <div className="input-group-addon">
-                                    <i className="fa fa-file-text-o" aria-hidden="true"></i></div>
-                                <input type="text" 
-                                    className="form-control" 
-                                    id="firstName" 
-                                    placeholder="John" 
-                                    onChange={ this.props.onChange } 
-                                    value={ this.props.searchBy.firstName } />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-xs-12">
-                        <div className="form-group">
-                            <label htmlFor="lastName">Last Name</label>
-                            <div className="input-group">
-                                <div className="input-group-addon">
-                                    <i className="fa fa-file-text-o" aria-hidden="true"></i>
-                                </div>
-                                <input type="text" 
-                                    className="form-control" 
-                                    id="lastName" 
-                                    placeholder="Doe" 
-                                    onChange={ this.props.onChange } 
-                                    value={ this.props.searchBy.lastName } />
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                
+                <div id="accordion">
 
-                {/* Submit button on row below search boxes */}
+                    <h2><span className="label label-default">Find By ID #</span></h2>
+                    <div className="row">
+                        <div className="col-xs-12">
+                            <div className="form-group">
+                                <label htmlFor="partyId">ID #</label>
+                                <div className="input-group">
+                                    <div className="input-group-addon">
+                                        <i className="fa fa-file-text-o" aria-hidden="true"></i>
+                                    </div>
+                                    <input type="text" 
+                                        className="form-control" 
+                                        id="partyId" 
+                                        placeholder="123" 
+                                        onChange={ this.props.onChange } 
+                                        value={ this.props.searchBy.partyId } />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <h2><span className="label label-default">Find By Name</span></h2>
+                    <div className="row">
+                        <div className="col-lg-6 col-xs-12">
+                            <div className="form-group">
+                                <label htmlFor="firstName">First Name</label>
+                                <div className="input-group">
+                                    <div className="input-group-addon">
+                                        <i className="fa fa-file-text-o" aria-hidden="true"></i></div>
+                                    <input type="text" 
+                                        className="form-control" 
+                                        id="firstName" 
+                                        placeholder="Jane"  
+                                        onChange={ this.props.onChange } 
+                                        value={ this.props.searchBy.firstName } />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-6 col-xs-12">
+                            <div className="form-group">
+                                <label htmlFor="lastName">Last Name</label>
+                                <div className="input-group">
+                                    <div className="input-group-addon">
+                                        <i className="fa fa-file-text-o" aria-hidden="true"></i>
+                                    </div>
+                                    <input type="text" 
+                                        className="form-control" 
+                                        id="lastName" 
+                                        placeholder="Doe"  
+                                        onChange={ this.props.onChange } 
+                                        value={ this.props.searchBy.lastName } />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                
+                {/* Submit button is outside the on row below search boxes */}
                 <div className="row">
                     <div className="col-lg-1 col-xs-6">
                         <button className="btn btn-primary" type="submit">Submit</button>
@@ -76,9 +90,12 @@ var SearchForm = React.createClass({
                         <button className="btn btn-primary" type="reset">Reset</button>
                     </div>
                 </div>
+
             </form>
         );
+        
         /* jshint ignore:end */
+        
     }
     
 });
