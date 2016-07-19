670f3b924a18aa4d717da525de79abcaad32f002
brichett <brichett13@gmail.com>
2016-07-19 14:17:57 -0500

    Redis is now implemented in accountApi.

--- src/api/accountApi.js
+++ src/api/accountApi.js
@@ -6,7 +6,8 @@
 /////////////////////////////////////////////////
 
 /* jshint shadow:true */
-
+var redisClient = require('../config/redisClient');
+var winston = require('winston');
 var accountApi = function (knex) {
 
     var accountController = require('../controllers/accountController')(knex);
@@ -48,18 +49,38 @@ var accountApi = function (knex) {
     var getAccounts = function (req, res) {
         //If nothing is specified in the query string, use getAccountsByOwner as the default
         if (Object.keys(req.query).length === 0) {
-
-            var resultsForThisUser = accountController.getAccountsByOwner(req.user);
-            // IF ELSE block interprets controller returning an object or null
-            if (resultsForThisUser === null) {
-                res.json({
-                    'message': 'You do not have permission to own accounts!'
-                });
-            } else {
-                resultsForThisUser.then(function (accounts) {
-                    res.json(accounts);
-                });
-            }
+            //Since we are retrieving a data list that would be both accessed frequently and changed rarely, implementing caching here is preferred for performance optimization. 
+            //First we check whether the data being requested is already in the cache. If it is, we send it along.
+            //If it isn't in the cache, then we proceed to calling the controller layer for the normal process, and then we end by copying that data into the cache. 
+            //We only implement redis caching for the getByOwner method here, because search requests for specific accounts would not be made nearly as frequently as requests for My Accounts. 
+            var redis = redisClient.getClient();
+            //create a unique key for this cache
+            var cacheKeyName = 'accounts_for_partyId_' + req.user.partyId;
+            redis.get(cacheKeyName, function (err, result) {
+                if (result) {
+                    //We found the desired data in the cache. Parse it into valid JSON and send it along.
+                    res.json(JSON.parse(result));
+                }
+                else {
+                    winston.error('No redis');
+                    //Proceed with calling the controller layer as normal.
+                    var resultsForThisUser = accountController.getAccountsByOwner(req.user);
+                    // IF ELSE block interprets controller returning an object or null
+                    if (resultsForThisUser === null) {
+                        res.json({
+                            'message': 'You do not have permission to own accounts!'
+                        });
+                    }
+                    else {
+                        resultsForThisUser.then(function (accounts) {
+                            redis.setex(cacheKeyName, 60, JSON.stringify(accounts));
+                            res.json(accounts);
+                        });
+                        
+                    }
+                }
+            });
+            
         }
         //If query strings are non-empty, use getAccountsByIdentity
 
@@ -71,7 +92,7 @@ var accountApi = function (knex) {
             var resultForThisAccount = accountController.getAccountByPhoneNumber(req.query, req.user);
             if (resultForThisAccount === null) {
                 res.json({
-                    'message': 'You do not have permission to own phonenumber by accounts!'
+                    'message': 'You do not have permission to view that account!'
                 });
             } else {
                 resultForThisAccount.then(function (accounts) {
@@ -85,7 +106,7 @@ var accountApi = function (knex) {
                 accountController.getAccountsByIdentity(req.query, req.user);
             if (resultForThisAccount === null) {
                 res.json({
-                    'message': 'You do not have permission to own identiy by accounts!'
+                    'message': 'You do not have permission to view that account!'
                 });
             } else {
                 resultForThisAccount.then(function (accounts) {
