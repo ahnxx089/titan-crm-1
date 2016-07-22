989cb1b59916977ef87966cbfa7f09f928765f91
dpshenoy <dpshenoy@comcast.net>
2016-07-22 15:19:59 -0500

    Modified redisConfig.js, still attempting to get Redis caching on Azure deployed app

--- src/config/redisClient.js
+++ src/config/redisClient.js
@@ -22,8 +22,9 @@ var redisClient = (function () {
     };
 
     // Attempt to create a new instance of an actual redis client
-    var connectionString = process.env.REDIS_URL || 'redis://localhost:6379';
-    console.log('connectionString = ',connectionString);
+    //
+    /* (This is from the Heroku deployment, commented out in favor of Azure)
+    //var connectionString = process.env.REDIS_URL || 'redis://localhost:6379';
     var c = redis.createClient(connectionString, {
         retry_strategy: function (options) {
             if (options.error.code === 'ECONNREFUSED') {
@@ -33,6 +34,24 @@ var redisClient = (function () {
             }
         }
     });
+    */
+
+    // This is the Azure-deployed Redis cache, attempt to make a new instance,
+    // including the optional retry_strategy.
+    // Redis docs:  https://github.com/NodeRedis/node_redis
+    // Azure docs:  https://azure.microsoft.com/en-us/documentation/articles/cache-nodejs-get-started/
+    var host = 'titan-crm.redis.cache.windows.net';
+    var port = 6379;
+    var primaryKey = '8H6lQayUcWTnzqxDgwmi9XDmIw44zD/kyrjN1yOd32U=';
+    var options = {
+        auth_pass: primaryKey,
+        tls: {
+            servername: host
+        }
+    }
+    console.log('port, host, options = ', port, host, options);
+
+    var c = redis.createClient( port, host, options );
 
     // Set the "client" variable to the actual redis client instance
     // once a connection is established with the Redis server
