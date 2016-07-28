c7abf0c5534d01796af89cfc93a667641de624b8
dpshenoy <dpshenoy@comcast.net>
2016-07-28 17:56:29 -0500

    Added pool option with a ping function to knexConfig.js per Pool2 library docs, see if this fixes Azure deployed app's recurring Knex:warning - Pool2 - Error: read ECONNRESET and/or Knex:warning - Pool2 - Error: Quit inactivity timeout

--- src/config/knexConfig.js
+++ src/config/knexConfig.js
@@ -11,11 +11,14 @@ var knexConfig = function() {
     var getConfig = function() {
         return knex({
             client: 'mysql',
-            connection: process.env.TITANCRM_DB_URL || 'mysql://root:@localhost/titan_crm'
-            // env var TITANCRM_DB_URL is for Azure deployment, prioritize over local db.
-            // Previously for Heroku deploy:
-            //connection: process.env.JAWSDB_URL || 'mysql://root:@localhost/titan_crm'
-            // connection string (one liner way to how to connect): type of connect://userName:password@host/databaseName
+            connection: process.env.TITANCRM_DB_URL || 'mysql://root:@localhost/titan_crm',
+            pool: {
+                min: 2,
+                max: 10,
+                ping: function (resource, callback) {
+                    resource.ping(callback);
+                }
+            }
         });
     };
 
